c5291ec71cd2b2a7a46fff1f86227def6d7436c7
Ashmaawy <mohamedashmawy918@gmail.com>
2022-10-10 03:31:39 +0200

    initial commit

---
+++ main.dart
@@ -0,0 +1,33 @@
+void main() {
+  ///Data type
+//int => number  => 1 2 3 4 5 ...
+//double => number => 1.1  2.5  0.9 ...
+// bool  => true / false
+// String => 'mohamed'
+// var
+// dynamic
+
+  ///Conditions
+  //if
+  // if else
+  //if line   ?
+
+  /// Simple operators
+  // +
+  // -
+  // *
+  // /
+  // %
+
+  /// Loops
+
+  //for
+  //while
+
+  for (int i = 0; i <= 5; i++) {
+    print('Mohamed');
+  }
+
+
+
+}
